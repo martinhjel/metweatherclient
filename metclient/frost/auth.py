7f94ec05089954062a2423ed5e4a99f37711a6ed
Martin Hjelmeland <marhje90@gmail.com>
2019-09-29 22:37:55 +0200

    added some classes and methods for authenticating and starting a session against the frost api

---
+++ metclient/frost/auth.py
@@ -0,0 +1,100 @@
+"""Module used for authentication."""
+import requests
+import json
+import configparser
+import logging
+from urllib.parse import urlencode
+
+from metclient.frost.lightning.lightning import Lightning
+
+class ConfigException(Exception):
+    pass
+
+class ConfigMissingException(Exception):
+    pass
+
+class ApiEndpoints:
+    BASE_FROST_ENDPOINT = "https://frost.met.no/"
+    API_VERSION = 'v0'
+    OUTPUT_FORMATS = ['jsonld', 'ualf']
+    TYPE = [
+        'observations', 'climatenormals', 'frequencies', 'records', 'lightning',
+        'locations', 'elements', 'sources'
+    ]
+
+    @classmethod
+    def lightning(cls):
+        return ApiEndpoints.BASE_FROST_ENDPOINT + 'lightning/' + ApiEndpoints.API_VERSION + '.' + 'ualf'
+
+
+class Auth:
+    """
+    Authentication
+    """
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self._validate()
+
+    def _validate(self):
+
+        auth = (self.client_id, self.client_secret)
+
+        # TODO: Add validation step
+
+        return True
+
+    def get_auth(self):
+        return self.client_id, self.client_secret
+
+
+class Session(ApiEndpoints):
+
+    def __init__(self, client_id=None, client_secret=None, config_file=None, logger=None):
+        self.logger = logger or logging.getLogger(__name__)
+        self.auth = None
+        if config_file:
+            self.read_config_file(config_file)
+        elif client_id and client_secret:
+            self.config(client_id, client_secret)
+        else:
+            raise ConfigMissingException
+
+        self._session = requests.Session()
+        self._session.auth = self.auth.get_auth()
+
+    def read_config_file(self, config_file):
+        config = configparser.ConfigParser()
+        config.read(config_file)
+
+        self.auth = Auth(
+            config['credentials']['client_id'],
+            config['credentials']['client_secret']
+        )
+
+    def config(self, client_id, client_secret):
+        if self.auth:
+            raise ConfigException('Session already configured.')
+        self.auth = Auth(client_id, client_secret)
+
+    def query(self, endpoint, params):
+        response = self._session.get(endpoint +"?"+ urlencode(params))
+        response.raise_for_status()
+        return response
+
+    def get_data_frame(self, type, dict_params):
+
+        if type=='lightning':
+            response = self.query(
+                self.lightning(),
+                dict_params
+            )
+            return Lightning.get_df(response)
+
+    def get_metadata(self):
+        pass
+
+    def search(self):
+        pass
+
+
